de599b4b553c5f9fcefb44f2fe16c7de8d35c4aa
Shazam32918 <gomezgonzalezandressimonei@gmail.com>
2025-04-05 17:20:42 -0400

    Update inio.sh

--- inio.sh
+++ inio.sh
@@ -3,6 +3,5 @@ sudo dpkg -i jdk-21_linux-x64_bin.deb
 mkdir server
 cd server
 wget https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/222/downloads/paper-1.21.4-222.jar
-echo "eula=true" > cloudminecraft/server/eula.txt
-mv paper-1.21.4-222.jar servidor.jar
+echo "eula=true" > /server/eula.txt
 
